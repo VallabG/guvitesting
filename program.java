d5169f41c082954660041d6cb9c7014ddbbab771
VallabG <33744884+VallabG@users.noreply.github.com>
2017-11-17 12:57:26 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,22 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number = 2;
+		
+	if (number > 0) {
+
+            System.out.println(number + " is a positive number");
+
+        } else if (number < 0) {
+
+            System.out.println(number + " is a negative number");
+
+        } else {
+
+            System.out.println(number + " is neither positive nor negative");
+
+        }
+
+	}
+}
